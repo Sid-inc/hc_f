bcb4218fcb0c87727baf9019f484c0d3caaee29b
ionovnn <ionovnn@monqlab.com>
2022-03-17 19:21:47 +0300

    Добавлен виджет телефона

--- src/app/dashboard/widget/widget.component.ts
+++ src/app/dashboard/widget/widget.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+
+interface Widget {
+  name: string;
+  state: boolean;
+  icon?: string;
+}
+
+class Phone implements Widget {
+  public name: string;
+  public state: boolean;
+  public icon?: string;
+
+  constructor(name: string, state: boolean, icon?: string) {
+    this.name = name;
+    this.state = state;
+    this.icon = '../../../assets/images/phone.png';
+  }
+}
       
 @Component({
     selector: 'app-widget',
     templateUrl: "./widget.component.html",
     styleUrls: ["./widget.component.scss"]
 })
-export class WidgetComponent { }
+export class WidgetComponent {
+  MyPhone = new Phone('MyPhone', false);
+}
